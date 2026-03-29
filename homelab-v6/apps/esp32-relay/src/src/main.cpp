12764be165cdf531a996c3fbf951a14e89e8281c
wguilherme <wguilhermecriativo@gmail.com>
2026-03-29 11:50:39 -0300

    feat: separa valores do delay interval e serial boot delay em constantes reutilizáveis

--- homelab-v6/apps/esp32-relay/src/src/main.cpp
+++ homelab-v6/apps/esp32-relay/src/src/main.cpp
@@ -1,28 +1,22 @@
 #include <Arduino.h>
 
-// Firmware de verificação do pipeline Tekton CI/CD
-// Relé conectado ao GPIO23 (3v3 + GND)
 #define FIRMWARE_VERSION "1.3.0"
 #define RELAY_PIN 23
+#define RELAY_DELAY_INTERVAL 500
+#define SERIAL_BOOT_DELAY 1000
 
 void setup() {
   Serial.begin(115200);
-  delay(2500);
+  delay(SERIAL_BOOT_DELAY);
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, LOW);
-
-  Serial.println("\n================================");
-  Serial.println("  ESP32 WROOM - Tekton CI/CD   ");
-  Serial.printf ("  Version: %s\n", FIRMWARE_VERSION);
-  Serial.println("  Relay: GPIO23               ");
-  Serial.println("================================\n");
 }
 
 void loop() {
   Serial.printf("[v%s] relay ON\n", FIRMWARE_VERSION);
   digitalWrite(RELAY_PIN, HIGH);
-  delay(2500);
+  delay(RELAY_DELAY_INTERVAL);
   Serial.printf("[v%s] relay OFF\n", FIRMWARE_VERSION);
   digitalWrite(RELAY_PIN, LOW);
-  delay(2500);
+  delay(RELAY_DELAY_INTERVAL);
 }
